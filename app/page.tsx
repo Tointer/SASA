271b193bc6aeb7e3957c1b99fbba5cec6b53777e
Tointer <Tointer@yandex.ru>
2025-05-22 05:17:30 +0300

    add more examples on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
           <p className='mt-4 text-xl'>SASA is making security for both humans and AI agents more reachable</p>
           <p className='mt-2 text-xl'>It will analyse onchain data, scan transaction for any suspicious activity and explain everything in human readable way</p>
         </div>
+        
         <Blob></Blob>
+
         <section >
           <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
               <div className="space-y-8">
@@ -24,6 +26,34 @@ export default function Home() {
               </div>
           </div>
         </section>
+
+        <section >
+          <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+              <div className="mt-4 md:mt-0">
+                  <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">It will warn if something is off</h2>
+                  <p className="mb-6 font-light  md:text-lg text-gray-400">SASA have additional mechanisms to see if transaction is suspicious. There are several systems that will scan transaction</p>
+              </div>
+              <div className="space-y-8">
+                <DialogBox  title="Are you sure?" message="Your transaction are interacting with contract that have name of some popular contract, but with different address.
+        This can be a sign of scam, or some new version of contract, please double check before proceeding" cat={ResponseCategory.warning}/>
+                <DialogBox title="Contract is new" message="You are interacting with contract that was created recently, please double check if you know this contract" cat={ResponseCategory.warning}/>
+              </div>
+          </div>
+        </section>
+
+        <section >
+          <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+              <div className="space-y-8">
+                <DialogBox title="Scam alert!" message="Your transaction is interacting with scam address, do NOT proceed!" cat={ResponseCategory.alarm}/>
+                <DialogBox title="Compromised contract!" message="You are interacting with compromised contract, do NOT proceed!" cat={ResponseCategory.alarm}/>
+              </div>
+              <div className="mt-4 md:mt-0">
+                  <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">It will protect</h2>
+                  <p className="mb-6 font-light  md:text-lg text-gray-400">We do not need multiple users to interact with dangerous website. If just one will report it, we can mark it and prevent users from losing funds</p>
+              </div>
+          </div>
+        </section>
+
         <h1 className='text-4xl font-bold m-8'>
           Want to test how it work? Here is <a href="/interface/tx-ask" className='text-blue-600  hover:underline'>web interface demonstration</a>
         </h1>
